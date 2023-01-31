ca94e06ddd26967ef7819416d02f81867634cca8
sebastian medina <sebastianlopez_21@hotmail.com>
2023-01-30 22:48:32 -0500

    commit tree c7bcbf04c016df38cc6a04e172b06601aeeb20db

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :anagram_word
+
+    def initialize(anagram_word)
+        @anagram_word = anagram_word
+    end
+
+    def match(array_words)
+        matched_words = []
+        array_words.map do |word|
+             if word.chars.sort == anagram_word.chars.sort
+                  matched_words << word   
+             end
+             
+        end
+        return matched_words
+    end
+end
+
+listen = Anagram.new("listen")
+
+listen.match(%w[enlists google inlets banana])
